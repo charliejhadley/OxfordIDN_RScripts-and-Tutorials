ce58ec20558552d161c5f233f8c36116d2c2dee2
martinjhnhadley <martinjohnhadley@gmail.com>
2016-08-05 09:31:06 +0100

    .gitignore is now working

--- Repositories/Figshare/Figshare_Cache_Example/ui.R
+++
@@ -1,9 +0,0 @@
-shinyUI(
-  fluidPage(
-    wellPanel(
-      includeMarkdown("App_Description.Rmd"),
-              actionButton("update", label = "update")),
-    # dataTableOutput("summary"),
-    uiOutput("summary")
-  )
-)
--- Shiny-Templates/Leaflet/README.md
+++
@@ -1,5 +0,0 @@
-Interactive Leaflet Map
-=============
-
-Please note that the code herein is forked from https://github.com/lcyraphael/ok_shinymap
-
